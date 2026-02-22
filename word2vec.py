27f8751a7860bbc889c0792b281da1b502203eeb
Roman Tomchik <welldefinedkernel@gmail.com>
2026-02-22 13:13:37 +0100

    Implement forward pass, add attributes

--- word2vec.py
+++ word2vec.py
@@ -1,9 +1,16 @@
 import numpy as np
+import re
 
 class Word2Vec:
-    def __init__(self, vocab_size, embedding_dim, learning_rate=1e-3, negative_samples=5):
+    def __init__(self, 
+                 vocab_size, 
+                 embedding_dim, 
+                 learning_rate=1e-3, 
+                 window_size = 5,
+                 negative_samples=5):
+
         self.E = np.ones((vocab_size, embedding_dim)) # Input embeddings
-        self.W = np.ones((embedding_dim, vocab_size)) # Output embeddings
+        self.W = np.ones((vocab_size, embedding_dim)) # Output embeddings
         
         self.v = vocab_size
         self.d = embedding_dim
@@ -11,26 +18,47 @@ class Word2Vec:
         self.alpha = learning_rate
         self.k = negative_samples
 
+        self.C = window_size # Max amount of words from context to train on
+
     def _sigmoid(self, x):
+        return np.reciprocal(1 + np.exp(-x))
+
+    def _get_negative_samples(self, center_idx, context_idx, n_samples):
         pass
 
-    def _get_negative_samples(self, context_idx, n_samples):
-        pass
+    def loss(self, cache):
+        e_w = cache["emb"]
+        v_ci = cache["pos"]
+        v_cjs = cache["neg"]
+
+        pos_loss = -np.log(self._sigmoid(np.dot(e_w, v_ci)))
+        neg_loss = -np.sum([np.log(1 - self._sigmoid(np.dot(e_w, v_cj))) for v_cj in v_cjs])
+
+        L = pos_loss + neg_loss
+
+        return L
 
     def forward(self, center_idx, context_idx, negative_indices):
+        e_w = self.E[center_idx, :]                      # Embedding vector
+        v_ci = self.W[context_idx, :]                    # Positive sample 
+        v_cjs = [self.W[i, :] for i in negative_indices] # Negative samples
+
+        return {"emb" : e_w, "pos" : v_ci, "neg" : v_cjs}
+
+    def backward(self, cache):
         pass
 
-    def backward(self, center_idx, context_idx, negative_indices, cache):
-        pass
-
-    def train(self, corpus, epochs):
-        pass
+    def train(self, corpus: str, epochs=3):
+        for e in range(epochs):
+            ...
 
     def get_embedding(self, word_idx):
-        pass
+        return self.E[word_idx, :] 
 
 def main():
-    pass
+    word2vec = Word2Vec(10, 10)
+    cache = word2vec.forward(5, 5, [0, 1, 2, 3, 4])
+    print(word2vec.loss(cache))
 
 if __name__ == "__main__":
     main()
